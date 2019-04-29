09d2b96c5302f4640eec176fda7183f5921f1d53
Pieter O'Hearn <pieter.ohearn@gmail.com>
2019-04-29 11:02:23 +0200

    Syntax
    Syntax error

--- HealthCareSystem/src/test/java/StepDefinition.java
+++ HealthCareSystem/src/test/java/StepDefinition.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import hospitalmanagementsystem.*;
+import hospitalmanagementsystem.departments.Emergency;
 import hospitalmanagementsystem.users.*;
 
 public class StepDefinition {
