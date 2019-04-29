89c56a1b83562aafaa707d463e1a55c2ba06a3d0
karolineoest <48473328+karolineoest@users.noreply.github.com>
2019-04-29 09:15:43 +0200

    Add files via upload

--- HealthCareSystem/src/main/java/hospitalmanagementsystem/Query.java
+++ HealthCareSystem/src/main/java/hospitalmanagementsystem/Query.java
@@ -23,58 +23,58 @@ public static Query getInstance()
     return single_instance; 
 }
 //get number of patients currently in each department
-public int inSize = Inpatient.getInstance().getPatientList().size();
-public int outSize = Outpatient.getInstance().getPatientList().size();
-public int emSize = Emergency.getInstance().getPatientList().size();
+public static int inSize = Inpatient.getInstance().getPatientList().size();
+public static int outSize = Outpatient.getInstance().getPatientList().size();
+public static int emSize = Emergency.getInstance().getPatientList().size();
 //get max and min number of patients
-List<Integer> patientSizes = Arrays.asList(inSize,outSize,emSize);
-int  maxPatients= Collections.max(patientSizes);
-int minPatients = Collections.min(patientSizes);
+static List<Integer> patientSizes = Arrays.asList(inSize,outSize,emSize);
+static int  maxPatients= Collections.max(patientSizes);
+static int minPatients = Collections.min(patientSizes);
 
 //get number of users currently in each department
-public int inUsers = Inpatient.getInstance().getUserList().size();
-public int outUsers = Outpatient.getInstance().getUserList().size();
-public int emUsers = Emergency.getInstance().getUserList().size();
-public int manUsers = Management.getInstance().getUserList().size();
+public static int inUsers = Inpatient.getInstance().getUserList().size();
+public static int outUsers = Outpatient.getInstance().getUserList().size();
+public static int emUsers = Emergency.getInstance().getUserList().size();
+public static int manUsers = Management.getInstance().getUserList().size();
 //get max and min number of users
-List<Integer> userSizes = Arrays.asList(inSize,outSize,emSize);
-int  maxUsers = Collections.max(userSizes);
-int minUsers = Collections.min(userSizes);
+static List<Integer> userSizes = Arrays.asList(inSize,outSize,emSize);
+static int  maxUsers = Collections.max(userSizes);
+static int minUsers = Collections.min(userSizes);
 //Save department names as Strings
-List<String> departments = Arrays.asList("Inpatient","Outpatient","Emergency","Management");
+static List<String> departments = Arrays.asList("Inpatient","Outpatient","Emergency","Management");
 
-public String depMostPatients() {
+public static String depMostPatients() {
 	//department with most admitted patients
 	
 	int i = patientSizes.indexOf(maxPatients);
 	
 	return departments.get(i);
 }
-public String depLeastPatients() {
+public static String depLeastPatients() {
 	//least patients 
 	
 	int i = patientSizes.indexOf(minPatients);
 	
 	return departments.get(i);
 }
-public String depMostUsers() {
+public static String depMostUsers() {
 	//department with most admitted patients
 	
 	int i = userSizes.indexOf(maxUsers);
 	
 	return departments.get(i);
 }
-public String depLeastUsers() {
+public static String depLeastUsers() {
 	//least patients 
 	int i = userSizes.indexOf(minUsers);
 	
 	return departments.get(i);
 }
-public int totPatients() {
+public static int totPatients() {
 	//get total number of patients in entire hospital
 	return inSize+emSize+outSize;
 }
-public int totUsers() {
+public static int totUsers() {
 	//total number of users in entire hospital
 	return inUsers+outUsers+emUsers;
 }
