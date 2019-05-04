a46e915193d83ad7f7fe9b74bfeeb448adee0c5a
s151607 <48469702+s151607@users.noreply.github.com>
2019-05-04 16:48:09 +0200

    Update PersistenceLayerTest.java

--- HealthCareSystem/src/test/java/PersistenceLayerTest.java
+++ HealthCareSystem/src/test/java/PersistenceLayerTest.java
@@ -1,332 +1,201 @@
-package hospitalmanagementsystem;
+import static org.junit.Assert.*;
 
-import java.beans.XMLDecoder;
-import java.beans.XMLEncoder;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import hospitalmanagementsystem.departments.Department;
-import hospitalmanagementsystem.users.User;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import hospitalmanagementsystem.*;
+import hospitalmanagementsystem.departments.*;
+import hospitalmanagementsystem.users.*;
 
 /**
- * @author Asger Conradsen (S151607)
+ *
+ * @author Asger Conradsen
  */
-public class PersistenceLayer {
-	// STATIC VARIABLES
+ public class PersistenceLayerTest{
+	 
+	static PersistenceLayer persist;
+	
+	static Emergency em;
+	static Inpatient inPa;
+	static Outpatient outPa;
+	static Management man;
+	 
+	static Admin admin;
+	static Doctor doc;
+	static Nurse nurse;
+	static User user;
+	
+	static Bed b1;
+	
+	static Patient p1;
+	static Patient p2;
+	static Patient p3;
+	static Patient p4;
+	
 	static final String[] departments = {"Emergency", "Inpatient", "Outpatient", "Management", "Temp"};
 	static final String[] subfolders = {"Users", "Patients", "Beds"};
-
-	// CONSTRUCTOR
+	
 	/**
-	 * Default constructor for the PersistencyLayer that creates the
-	 * folder structure if it doesn't already exist.
+	 * Initializes objects with values used for testing
+	 * @throws IllegalAccessException
 	 */
-	public PersistenceLayer() {
-		// Create a Folder called Departments
-		File folder = new File("Departments");
-		folder.mkdir();
+	@BeforeClass
+	public static void setUpBeforeClass() throws IllegalAccessException {
+		persist = new PersistenceLayer();
+
+		em = Emergency.getInstance();
+		inPa = Inpatient.getInstance();
+		outPa = Outpatient.getInstance();
+		man = Management.getInstance();
 		
-		// inside Departments make a folder for each department
-		for (String department : departments) {
-			folder = new File("Departments" + File.separator + department);
-			folder.mkdir();
+		p1 = new Patient("Asger", "Conradsen", "04/10/1995", "487 downtown 2800 Kongens Lyngby", "+4585476964");
+		p1.updateDepartment(em);
+		
+		admin = new Admin("Steve Jobs","+180249625");
+		doc = new Doctor("Dr. Smith", "+4545252525", "Emergency");
+		nurse = new Nurse("John Doe", "+4512345678", "Emergency");
+		user = new User("James Gosling", "+141558396");
+		
+		b1 = new Bed(em.getName());
+	}
+	 
+	/**
+	 * Cleans folders before each test
+	 */
+	@Before
+	public void clean() {
+		for(String depart : departments) {
+			String dir = "Departments" + File.separator + depart + File.separator;
 			
-			if(department.equals("Management")) {
-				// inside the Management folder make a folder Users
-				folder = new File("Departments" + File.separator + department + File.separator + "Users");
-				folder.mkdir();
-			} else if(department.equals("Outpatient")) {
-				// inside the Outpatient folder make folders for the Users and Patients
-				folder = new File("Departments" + File.separator + department + File.separator + "Users");
-				folder.mkdir();
-				folder = new File("Departments" + File.separator + department + File.separator + "Patients");
-				folder.mkdir();
-			} else {
-				// inside each department folder make folders for the Users, Beds and Patients
-				for (String subfolder : subfolders) {
-					folder = new File("Departments" + File.separator + department + File.separator + subfolder);
-					folder.mkdir();
+			for(String folder : subfolders) {
+				File delDir = new File(dir + folder);
+				if(delDir.exists()) {
+					for(File file: delDir.listFiles()) {
+						file.delete();
+					}
 				}
-			}
-		}
+			}	
+		}			
 	}
 
-	// METHODS
-	/**
-	 * 
-	 * @param department
+	
+	 /**
+	 * Tests the save method of PersistenceLayer.
+	 * Note that save is overloaded and returns a bool (true when succesfully saved)
+	 * so that is what we check for.
 	 */
-	public void loadDepartment(Department department) {
-		//Loads users in the department
-		ArrayList<Object> loadedUsers = loadObjs(department, "users");
-		if(!loadedUsers.isEmpty()) {
-			ArrayList<User> users = new ArrayList<User>();
-			for(Object o : loadedUsers) {
-				users.add((User) o);
-			}
-			department.setUserList(users);
-		}
-
-		//Loads patients in the department
-		ArrayList<Object> loadedPatients = loadObjs(department, "patients");
-		if(!loadedPatients.isEmpty()) {
-			ArrayList<Patient> patients = new ArrayList<Patient>();
-			for(Object o : loadedPatients) {
-				patients.add((Patient) o);
-			}
-			department.setPatientList(patients);
-		}
-
-		//Loads beds in the department
-		ArrayList<Object> loadedBeds = loadObjs(department, "beds");
-		if(!loadedBeds.isEmpty()) {
-			ArrayList<Bed> beds = new ArrayList<Bed>();
-			for(Object o : loadedBeds) {
-				beds.add((Bed) o);
-			}
-			department.setBedList(beds);
-		}
-	}
-
-	/**
-	 * Returns an ArrayList of either users, patients or beds in the given departmen
-	 * based on which "type" is passed to the method.
-	 */
-	private ArrayList<Object> loadObjs(Department department, String type){
-		// create an ArrayList and set type to LowerCase
-		ArrayList<Object> objs = new ArrayList<Object>();
-		type = type.toLowerCase();
+	@Test
+	public void saveTest(){
+		//Saving a patient
+		assertEquals(true, persist.save(p1, p1.getPatientID(), em.getName()));
 		
-		// if not a valid load then return the empty list
-		if((department.getName().equals("Management") && (type.equals("patients") || type.equals("beds")))
-				|| (department.getName().equals("Outpatient") && type.equals("beds"))) {
-			return objs;
-		}
+		//Saving users
+		assertEquals(true, persist.save(admin, admin.getUserID(), em.getName()));
+		assertEquals(true, persist.save(doc, doc.getUserID(), outPa.getName()));
+		assertEquals(true, persist.save(nurse, nurse.getUserID(), outPa.getName()));
+		assertEquals(true, persist.save(user, user.getUserID(), man.getName()));
+		assertEquals(false, persist.save(user, "k123", man.getName()));
 		
-		// set the file directory string
-		String dir = "Departments" + File.separator + department.getName();
-		switch (type) {
-			case "users":
-				dir = dir + File.separator + "Users";
-				break;
-			case "patients":
-				dir = dir + File.separator + "Patients";
-				break;
-			case "beds":
-				dir = dir + File.separator + "Beds";
-				break;
-			default:
-				return objs;
-		}
-
-		// Gets a list of files in the directory and checks if they are valid
-		XMLDecoder d = null;
-		File[] files = new File(dir).listFiles();
-
-		// go through each file and try and load them
-		for(File file : files) {
-			if(file.isFile()) {
-				try{
-					d = new XMLDecoder(
-		                new BufferedInputStream(
-		                    new FileInputStream(dir + File.separator + file.getName())));
-				} catch(FileNotFoundException fileNotFound) {
-					System.out.println(fileNotFound.getMessage());
-				}
-				// read the object and add to the list
-				Object obj = d.readObject();
-				objs.add(obj);
-			}
-		}
-
-		return objs;
+		//Saving a bed
+		assertEquals(true, persist.save(b1, b1.getBedID(), em.getName()));
 	}
 	
 	/**
-	 * Loads the Temp
-	 * @return
+	 * Simple test for saving the ID counter.
 	 */
-	public ArrayList<Object> loadTemps(char type) {
-		
-		// Creates directory path for the department
-		String dir = "Departments" + File.separator + "Temp";
-		ArrayList<Object> objects = new ArrayList<Object>();
-
-		switch (type) {
-			case 'U':
-				dir = dir + File.separator + "Users";
-				break;
-			case 'P':
-				dir = dir + File.separator + "Patients";
-				break;
-			case 'B':
-				dir = dir + File.separator + "Beds";
-				break;
-			default:
-				return null;
-		}
-		
-		XMLDecoder d = null;
-		File[] files = new File(dir).listFiles();
-
-		for(File file : files) {
-			// Writes department object to the file specified by the directory
-			try{
-				d = new XMLDecoder(
-		                new BufferedInputStream(
-		                    new FileInputStream(dir + File.separator + file.getName())));
-			} catch(FileNotFoundException fileNotFound) {
-				System.out.println(fileNotFound.getMessage());
-			}
-			objects.add(d.readObject());
-		}
-		return objects;
-	}
-
-	/**
-	 * 
-	 * @return
-	 */
-	public int loadCounter() {
-		// Gets a list of files in the directory and checks if they are valid
-		XMLDecoder d = null;
-		String dir = "Departments" + File.separator + "Temp";
-		File[] files = new File(dir).listFiles();
-
-		Object obj = new Object();
-		
-		for(File file : files) {
-			if(file.getName().equals("Counter")) {
-				try{
-					d = new XMLDecoder(
-		                new BufferedInputStream(
-		                    new FileInputStream(dir + File.separator + file.getName())));
-				} catch(FileNotFoundException fileNotFound) {
-					System.out.println(fileNotFound.getMessage());
-				}
-				obj = d.readObject();
-				d.close();
-				return (int) obj;
-			}
-		}
-		// else return 0
-		return 0;
-	}
-
-	/**
-	 * Saves an object in a specified department to the appropriate folder. Same return as before.
-	 * @param obj
-	 * @param ID
-	 * @param department
-	 * @return
-	 */
-	public boolean save(Object obj, String ID, String department) {
-		// if department is null change to Temp
-		department = department==null?"Temp":department;
-		
-		// Creates the directory for the object to be stored
-		String dir = "Departments" + File.separator + department;
-		char type = ID.charAt(0);
-
-		// if type is another type of user set type to 'U'
-		if(type == 'A' || type == 'N' || type == 'D') {
-			type = 'U';
-		}
-
-		switch (type) {
-			case 'U':
-				dir = dir + File.separator + "Users";
-				break;
-			case 'P':
-				dir = dir + File.separator + "Patients";
-				break;
-			case 'B':
-				dir = dir + File.separator + "Beds";
-				break;
-			default:
-				return false;
-		}
-		dir = dir + File.separator + ID;
-
-		// Writes object to the file specified by the directory
-		XMLEncoder e = null;
-		try{
-			e = new XMLEncoder(
-                new BufferedOutputStream(
-                    new FileOutputStream(dir)));
-		} catch(FileNotFoundException fileNotFound) {
-			return false;
-		}
-		e.writeObject(obj);
-		e.close();
-		return true;
+	@Test
+	public void saveCounterTest(){
+		assertEquals(true, persist.saveCounter(5));
 	}
 	
 	/**
-	 * 
-	 * @param counter
-	 * @return
+	 * Test loadTemps by comparing with the id of the object we saved
 	 */
-	public Boolean saveCounter(int counter) {
-		// Creates directory path for the department
-		String dir = "Departments" + File.separator + "Temp";
-
-		// Writes department object to the file specified by the directory
-		XMLEncoder e = null;
-		try{
-			e = new XMLEncoder(
-                new BufferedOutputStream(
-                    new FileOutputStream(dir + File.separator + "Counter")));
-		} catch(FileNotFoundException fileNotFound) {
-			return false;
-		}
+	@Test
+	public void loadTempsTest() {
+		//Save objects in Temp
+		persist.save(doc, doc.getUserID(), "Temp");
+		persist.save(b1, b1.getBedID(), "Temp");
+		persist.save(p1, p1.getPatientID(), "Temp");
 		
-		e.writeObject(counter);
-		e.close();
-		return true;
+		//Test if lists matches against the ones we saved and loaded.
+		assertEquals(b1.getBedID(), ((Bed) persist.loadTemps('B').get(0)).getBedID());
+		assertEquals(doc.getUserID(), ((User) persist.loadTemps('U').get(0)).getUserID());
+		assertEquals(p1.getPatientID(), ((Patient) persist.loadTemps('P').get(0)).getPatientID());
+		
+		//Test for invalid input
+		assertEquals(null, persist.loadTemps('x'));	
 	}
-
+	
 	/**
-	 * 
-	 * @param ID
-	 * @param department
-	 * @return
+	 * Test loadDepartment by comparing with the id of the objects we saved
 	 */
-	public Boolean delete(String ID, String department) {
-		// if department is null set to "Temp"
-		department = (department==null) ? "Temp":department;
+	@Test
+	public void loadDepartmentTest(){		
+		//Save the objects
+		persist.save(b1, b1.getBedID(), em.getName());
+		persist.save(nurse, nurse.getUserID(), em.getName());
+		persist.save(p1, p1.getPatientID(), em.getName());
 		
-		// Creates the directory for the object to be stored
-		String dir = "Departments" + File.separator + department;
-		char type = ID.charAt(0);
+		//Load the depart and test if it matches against what we just saved
+		persist.loadDepartment(em);
 		
-		// if type is another type of user set type to 'U'
-		if(type == 'A' || type == 'N' || type == 'D') {
-			type = 'U';
-		}
+		assertEquals(b1.getBedID(), em.getBedList().get(0).getBedID());
+		assertEquals(nurse.getUserID(), em.getUserList().get(0).getUserID());
+		assertEquals(p1.getPatientID(), em.getPatientList().get(0).getPatientID());
 		
-		switch (type) {
-			case 'U':
-				dir = dir + File.separator + "Users";
-				break;
-			case 'P':
-				dir = dir + File.separator + "Patients";
-				break;
-			case 'B':
-				dir = dir + File.separator + "Beds";
-				break;
-			default:
-				return false;
-		}
-		dir = dir + File.separator + ID;
-
-		//Attempts to delete the file
-		File delFile = new File(dir);
-		if(delFile.delete()) return true;
-		else return false;
+		//load a department we didn't save anything in and check if empty
+		persist.loadDepartment(inPa);
+		assertEquals(true, inPa.getBedList().isEmpty());
+		assertEquals(true, inPa.getUserList().isEmpty());
+		assertEquals(true, inPa.getPatientList().isEmpty());		
 	}
-}
+	
+	/**
+	 * Test of the delete method. Done by testing if it returns true.
+	 */
+	@Test
+	public void deleteTest(){
+		//First save files so we can delete something.
+		p1.updateDepartment(inPa);
+		persist.save(p1, p1.getPatientID(), inPa.getName());
+		persist.save(b1, b1.getBedID(), inPa.getName());
+		persist.save(doc, doc.getUserID(), inPa.getName());
+		persist.save(admin, admin.getUserID(), inPa.getName());
+		persist.save(nurse, nurse.getUserID(), inPa.getName());
+		
+		//Delete users
+		assertEquals(true, persist.delete(p1.getPatientID(), inPa.getName()));
+		assertEquals(true, persist.delete(doc.getUserID(), inPa.getName()));
+		assertEquals(true, persist.delete(admin.getUserID(), inPa.getName()));
+		assertEquals(true, persist.delete(nurse.getUserID(), inPa.getName()));
+		
+		//Delete bed
+		assertEquals(true, persist.delete(b1.getBedID(), inPa.getName()));
+
+		//Try deleting file of unknown type
+		assertEquals(false, persist.delete("k123", inPa.getName()));
+		
+	}
+	 
+	/**
+	 * Test of the loadCounter method. Test against the counter we just saved.
+	 * Also has a testcase that deletes the saved counter and check if it correctly
+	 * returns 0.
+	 */
+	@Test
+	public void loadCounterTest() {
+		persist.saveCounter(5);
+		assertEquals(5, persist.loadCounter());
+		
+		File delFile = new File("Departments" + File.separator + "Temp" + 
+				File.separator + "Counter");
+		delFile.delete();
+		assertEquals(0, persist.loadCounter());	
+	}
+	 
+
+	 
+ }
